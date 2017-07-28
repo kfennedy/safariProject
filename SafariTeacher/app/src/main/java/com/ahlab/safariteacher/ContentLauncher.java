e6a9e8dfcc3de850705ffcf350d1ee98584c78cf
kfennedy <kfennedy@users.noreply.github.com>
2017-07-28 13:31:59 +0800

    remove zXing library since it is not accurate

--- SafariTeacher/app/src/main/java/com/ahlab/safariteacher/ContentLauncher.java
+++ SafariTeacher/app/src/main/java/com/ahlab/safariteacher/ContentLauncher.java
@@ -1,23 +1,24 @@
 package com.ahlab.safariteacher;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 /**
  * Created by Katherine on 28/7/17.
  */
 
-public class ContentLauncher extends ActivityLauncher {
+public class ContentLauncher extends ActivityLauncher{
 
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.update_content);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
     }
 }
