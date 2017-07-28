09efc681bfb06e0f5a6ca4910616263b86a525c0
kfennedy <kfennedy@users.noreply.github.com>
2017-07-28 09:51:17 +0800

    set up UI home page for teachers

---
+++ SafariTeacher/app/src/main/java/com/ahlab/safariteacher/HomeTeacher.java
@@ -0,0 +1,14 @@
+package com.ahlab.safariteacher;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeTeacher extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_teacher);
+    }
+}
